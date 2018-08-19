fd12aab7340e52fbb9e8f100725536b38e284ed6
Thomas Cheng <contact.chengthomas@gmail.com>
2018-08-19 13:01:22 -0400

    JavaPaint v.1.5
    Added a gradient feature, as well as cleaned up a bit of the code.

--- Shape.java
+++ Shape.java
@@ -6,8 +6,11 @@
  * Dsscription: This is the abstract Shape class.
  */
 
-import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GradientPaint;
+import java.awt.BasicStroke;
 
 abstract public class Shape{
     // creating the private variables for this class
@@ -15,17 +18,23 @@ abstract public class Shape{
     private int y1;
     private int x2;
     private int y2;
-    private Color color;
+    private Color colourOne;
+    private Color colourTwo;
+    private boolean isGradient;
+    private double lineSize;
     
     /* This is the constructor method for the Shape class. This method accepts 5 parameters. x1, y1, x2, y2,
      * representing the starting and ending coordinates of the shape respectively, and a reference to a Color object.
      */
-    public Shape( int x1, int y1, int x2, int y2, Color color ){
+    public Shape( int x1, int y1, int x2, int y2, Color colourOne, Color colourTwo, Boolean isGradient, double lineSize){
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.color = color;
+        this.colourOne = colourOne;
+        this.colourTwo = colourTwo;
+        this.isGradient = isGradient;
+        this.lineSize = lineSize;
     }
     
     /* This is the accessor method that returns the x1 value of the shape. This method accepts no values.
@@ -50,12 +59,6 @@ abstract public class Shape{
      */
     public int getY2(){
         return y2;
-    }
-  
-    /* This is the accessor method that returns the colour of the shape. This method accepts no values.
-     */
-    public Color getColor(){
-        return color;
     }
 
     /* This is the mutator method that changes the x1 value. This method accepts 1 parameter: an int value representing
@@ -86,12 +89,45 @@ abstract public class Shape{
         y2 = yValue;
     }
     
-    /* This is the mutator method that changes the shape's color. This method accepts 1 parameter: a reference to a
+    /* This is the mutator method that changes the shape's colour. This method accepts 1 parameter: a reference to a
      * Color object. This method returns no values.
      */
-    public void setColor( Color color ){
-        this.color = color;
+    public void setColourOne( Color colourOne ){
+        this.colourOne = colourOne;
     }
+    
+     /* This is the accessor method that returns the colour of the shape. This method accepts no values.
+     */
+    public Color getColourOne(){
+        return colourOne;
+    }
+    
+    public void setColourTwo( Color colourTwo ){
+        this.colourTwo = colourTwo;
+    }
+    
+    public Color getColourTwo( Color colourTwo ){
+        return colourTwo;
+    }
+    
+    public void setIsGradient ( boolean isGradient ) {
+        this.isGradient = isGradient;
+    }
+    
+    public boolean getIsGradient(){
+        return isGradient;
+    }
+    
+    public Graphics2D convertGraphics(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke( new BasicStroke( (float)lineSize ) );
+        if ( !isGradient )
+            colourTwo = colourOne;
+        GradientPaint gradient = new GradientPaint(x1, y1, colourOne, x2, y2, colourTwo);
+        g2d.setPaint(gradient);
+        return g2d;
+    }
+    
     
     /* This is the abstract draw method. This method accepts 1 parameter: a reference to a Graphics object. This method
      * returns no values.
--- SuperPaint.java
+++ SuperPaint.java
@@ -12,7 +12,7 @@ class SuperPaint {
     public static void main(String[] args) {
         DrawFrame application = new DrawFrame();
         
-        application.setSize( 600, 450 );
+        application.setSize( 1500, 700 );
         application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         application.setVisible( true );
     }
